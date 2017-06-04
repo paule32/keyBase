6df56d1209648e51c5e27da2769961c3dc95c7b3
paule32 <jkallup@web.de>
2017-06-04 15:16:30 +0200

    update

--- parser_sbase.cc
+++ parser_sbase.cc
@@ -2,9 +2,37 @@
 
 #include <iostream>
 #include <QString>
+#include <QVariant>
+#include <QMap>
 #include <QMessageBox>
 
 int line_no = 1;
+
+struct MyNode {
+    QVariant value;
+};
+QMap<QString, MyNode> app_node;
+
+const int TOK_PRINT = 1;
+
+bool handle_ident(QString ident, QVariant val)
+{
+    MyNode ptr_node;
+    int m = 0;
+    int i;
+    double d;
+
+    if (val.convert(QVariant::Int))    {m = 1; i = val.toInt();    }
+    if (val.convert(QVariant::Double)) {m = 2; d = val.toDouble(); }
+
+    switch (m) {
+        case 1: ptr_node.value = i; break;
+        case 2: ptr_node.value = d; break;
+    }
+
+    app_node.insert(ident,ptr_node);
+    return true;
+}
 
 bool parseText(char *buffer)
 {
@@ -72,6 +100,8 @@ bool parseText(char *buffer)
                     goto ende2;
                 }
                 else if (c == ' ' || c == '\t') {
+                    if (ident == QString("print"))
+                    goto ende2;
                     continue;
                 }
                 else if (c == '\n') {
@@ -102,12 +132,82 @@ bool parseText(char *buffer)
                 }   break;
             }
             ende2:
-            QMessageBox::information(0,"Infor",ident);
+            //QMessageBox::information(0,"Infor",ident);
+
+            // ----------------------
+            // print expr ...
+            // ----------------------
+            if (ident.toLower() == QString("print")) {
+                ident.clear();
+                while (1) {
+                    if (++pos >= strlen(buffer))
+                    return false;
+                    c = buffer[pos];
+                    if      (((c >= 'a') && (c <= 'z'))
+                         ||  ((c >= 'A') && (c <= 'Z'))
+                         ||  ((c >= '0') && (c <= '9'))
+                         ||   (c == '_')) {
+                        ident += c;
+                        while (1) {
+                            if (++pos >= strlen(buffer))
+                            goto ende3;
+                            c = buffer[pos];
+                            if (((c >= 'a') && (c <= 'z'))
+                                 ||  ((c >= 'A') && (c <= 'Z'))
+                                 ||  ((c >= '0') && (c <= '9'))
+                                 ||   (c == '_')) {
+                                ident += c;
+                                continue;
+                            }
+                            else if (c == ' ' || c == '\t') {
+                                goto ende3;
+                            }
+                            else if (c == '\n') {
+                                ++line_no;
+                                goto ende3;
+                            }
+                            else if (c == EOF) {
+                                goto ende3;
+                            }
+                        }
+                    }
+                }
+                ende3:
+                QMapIterator<QString, MyNode> iter(app_node);
+                bool found = false;
+                while (iter.hasNext()) {
+                    iter.next();
+                    if (iter.key() == ident) {
+                        QVariant val = iter.value().value;
+                        found = true;
+                        int m = 0;
+                        int i;
+                        double d;
+
+                        if (val.convert(QVariant::Int))    {m = 1; i = val.toInt();    }
+                        if (val.convert(QVariant::Double)) {m = 2; d = val.toDouble(); }
+
+                        QString str;
+                        switch (m) {
+                            case 1: str = QString("%1").arg(i); break;
+                            case 2: str = QString("%1").arg(d); break;
+                        }
+
+                        QMessageBox::information(0,"Print Var",
+                        QString("PRINT: %1").arg(str));
+
+                        break;
+                    }
+                }
+                // todo: v = 0;
+                if (found == false)
+                throw QString("Variable '%1' nicht definiert.").arg(ident);
+            }
             ident.clear();
             continue;
         }
         else if (c == '/') {
-        n5:
+        //n5:
             if (++pos >= strlen(buffer))
             return false;
             c = buffer[pos];
@@ -177,13 +277,17 @@ bool parseText(char *buffer)
                 }
             }
             ende1:
-            QMessageBox::information(0,"Info",
-            QString("==> %1").arg(number));
+            /*QMessageBox::information(0,"Info",
+            QString("%1 ==> %2")
+            .arg(ident)
+            .arg(number));*/
+
+            handle_ident(ident,  number);
+            ident .clear();
+            number.clear();
 
             if (c == EOF)
             return true;
-
-            number.clear();
             continue;
         }
         else if (c == EOF)  { return true; }
@@ -211,6 +315,12 @@ bool parseText(QString str)
         QString("Syntax Error in line: %1").arg(line_no));
         else
         QMessageBox::information(0,"Info","SUCCESS");
+    }
+    catch (QString &e) {
+        QMessageBox::critical(0,"Error",
+        QString("Error in line: %1\n%2")
+        .arg(line_no)
+        .arg(e));
     }
     catch (int &e) {
         if (e > 0) {
